5b32ce027c8dd0c1e2a45972ac20e8e95bc6d3f3
soymikey <soymikey@github.com>
2020-10-09 21:16:25 +0800

    update mongodb address

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ module.exports = {
   SALT_WORK_FACTOR: 10, // 生成salt的迭代次数
   TOKEN_SECRET: "secret-key",
   TOKEN_EXPIRESIN: "24h", // token 有效期
-  WEB_HOST: isDev ? "localhost:3000" : "http://pos.migaox.com", // 主机地址（端口）
+  WEB_HOST: isDev ? "localhost:4000" : "http://pos.migaox.com", // 主机地址（端口）
   DB: isDev
-    ? "mongodb://pos:pos335829832@34.84.145.207:3011/pos"
-    : "mongodb://dbBaseName:dbPassword@localhost:19999/pos",
+    ? "mongodb://pos:pos335829832@49.235.133.74:27016/pos"
+    : "mongodb://pos:pos335829832@49.235.133.74:27016/pos",
 };
